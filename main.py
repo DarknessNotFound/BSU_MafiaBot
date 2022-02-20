3d319e21b7323fbc9ab9e0298af07e108e7d88bd
Grant <galbrightr@gmail.com>
2022-02-19 22:01:14 -0600

    Slide into DMs working

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ GUILD = os.getenv('DISCORD_GUILD')
 
 client = discord.Client()
 
-bot = commands.Bot(command_prefix='--')
+bot = commands.Bot(command_prefix='-')
 
 #Prints a message to say it connected successfully
 #NOTE: Sometimes takes a second to trigger and send msg
@@ -24,15 +24,11 @@ async def on_ready():
 
 @bot.command(name='slideIntoDMs', help='Messages the user that typed the msg.')
 async def slideIntoDMs(ctx):
-    user = " "
     msg = "I just slid into your dms"
-    await user.send(msg)
-
+    await ctx.message.author.send(msg)
 
 @bot.command(name='echo', help='Echos what the user just said')
 async def echo(ctx, *args):
     await ctx.send(list_to_str(args))
-
-
 
 bot.run(TOKEN)
